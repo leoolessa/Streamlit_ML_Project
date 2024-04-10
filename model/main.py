54f7ab81159dffe3e2969d80d1cbdca7173fa815
leoolessa <70817999+leoolessa@users.noreply.github.com>
2024-04-10 02:20:11 +0200

    test

--- model/main.py
+++ model/main.py
@@ -17,7 +17,7 @@ def create_model(data):
         
     #split the data
     X_train, X_test, y_train, y_test = train_test_split(
-                X, y, test_size=0.30, random_state=42)
+                X, y, test_size=0.20, random_state=42)
         
     #train the model
     model = LogisticRegression()
